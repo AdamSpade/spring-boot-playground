3f2b10695591360737486821796951d55267bfd1
Adam Spade <spade.adam46@gmail.com>
2025-01-21 22:56:48 -0800

    Encrypted Database Credentials

--- src/main/java/com/example/mydemoapp/PlaygroundApplication.java
+++ src/main/java/com/example/mydemoapp/PlaygroundApplication.java
@@ -1,5 +1,6 @@
 package com.example.mydemoapp;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,10 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Simple app to demo various features of Java, Spring Boot, and more...
  */
 @SpringBootApplication
+@EnableEncryptableProperties
 public class PlaygroundApplication {
 
+    static final private String JASYPT_PROPERTY = "jasypt.encryptor.password";
+    static final private String JASYPT_ENCRYPTOR_PASSWORD = "DECRYPT_KEY";
+
     public static void main(String[] args) {
+        System.setProperty(JASYPT_PROPERTY, System.getenv(JASYPT_ENCRYPTOR_PASSWORD));
         SpringApplication.run(PlaygroundApplication.class, args);
     }
-
 }
